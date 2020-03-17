90f499acb3cc6ad0243493d68eb935d6522cb603
Miguel Angel Jimenez <miguejm@outlook.es>
2020-03-16 23:58:25 -0600

    Curso de javascript

---
+++ primero.js
@@ -0,0 +1,4 @@
+//console.log("Hola a todos :D");
+console.log("Hola mundo"); //imprime hola mundo en la consola
+alert("Hola Migue"); //Manda una alerta en el navegador
+prompt(); //Muestra una ventana(modal) que solicita un dato al usuario
